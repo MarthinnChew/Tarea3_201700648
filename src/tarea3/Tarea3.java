04dfdebfd9d465070492dece78cfd75a49166f3f
Marthinn Chew <Marthinn Chew@DESKTOP-J6VSV4B>
2018-08-18 01:41:08 -0600

    estoy trabajando en mi tarea

--- src/tarea3/Tarea3.java
+++ src/tarea3/Tarea3.java
@@ -4,18 +4,60 @@
  * and open the template in the editor.
  */
 package tarea3;
-
+import java.util.Scanner;
 /**
  *
  * @author Marthinn Chew
  */
-public class Tarea3 {
+public class Tarea3 {//link GitHub:https://github.com/MarthinnChew/Tarea3_201700648.git
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        int opcion = 0;
+        Scanner entrada = new Scanner(System.in);
+        
+                                                                      //menu principal
+        System.out.println("[IPC]Tarea3_201700648");
+        System.out.println("");
+        System.out.println("1.Usuario");
+        System.out.println("2.Contador de digitos repetidos");
+        System.out.println("3.Tres números de mayor a menor");
+        System.out.println("4.Calcular promedio");
+        System.out.println("5.Salir");                              //Aqui termina el menu principal
+        opcion = entrada.nextInt();
+        
+        if(opcion==1){//Usuario
+            System.out.println("1.Ingresar Usuario");
+            System.out.println("2.Mostrar Usuarios ascendente");
+            System.out.println("3.Mostrar Usuarios descendentes");
+            System.out.println("4.Menu principal"); 
+                    opcion = entrada.nextInt();
+                    if(opcion==1){//Ingresar Usuario
+                        
+                    }else if(opcion==2){//Mostrar Usuarios ascendente
+                    
+                    }
+            }else if(opcion==2){//Contador de digitos repetidos
+            System.out.println("1.Ingresar numero");
+            System.out.println("2.Ingresar numero a buscar");
+            System.out.println("3.Mostrar numero de digitos");
+            System.out.println("4.Menu principal");
+                    opcion = entrada.nextInt();
+            }else if(opcion==3){//Tres numeros de mayor a menor
+            System.out.println("1.Ingresar numero");
+            System.out.println("2.Mostrar ordenados");
+            System.out.println("3.Menu principal");
+                    opcion = entrada.nextInt();
+            }else if(opcion==4){//Calcular promedio(4notas de 6 estudiantes)
+                System.out.println("1.opcion");
+                System.out.println("2.opcion");
+            }else if(opcion==5){//5.Salir
+                System.out.println("finalizado");
+            }else if(opcion>5){
+                System.out.println("opcion erronia debe ser de 1 - 5");
+            }
+          
+    }//Finn Tarea3
 }
