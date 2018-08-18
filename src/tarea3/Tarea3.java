57fa99725212f1c6a15f62363d43322502d6e960
Marthinn Chew <Marthinn Chew@DESKTOP-J6VSV4B>
2018-08-18 10:12:29 -0600

    estoy trabajando en mi tarea

--- src/tarea3/Tarea3.java
+++ src/tarea3/Tarea3.java
@@ -18,7 +18,7 @@ public class Tarea3 {//link GitHub:https://github.com/MarthinnChew/Tarea3_201700
         int opcion = 0;
         Scanner entrada = new Scanner(System.in);
         
-                                                                      //menu principal
+                                                                  //menu principal
         System.out.println("[IPC]Tarea3_201700648");
         System.out.println("");
         System.out.println("1.Usuario");
@@ -28,36 +28,38 @@ public class Tarea3 {//link GitHub:https://github.com/MarthinnChew/Tarea3_201700
         System.out.println("5.Salir");                              //Aqui termina el menu principal
         opcion = entrada.nextInt();
         
-        if(opcion==1){//Usuario
+        switch(opcion){
+        case 1:// Menu Usuario
             System.out.println("1.Ingresar Usuario");
             System.out.println("2.Mostrar Usuarios ascendente");
             System.out.println("3.Mostrar Usuarios descendentes");
             System.out.println("4.Menu principal"); 
                     opcion = entrada.nextInt();
-                    if(opcion==1){//Ingresar Usuario
-                        
-                    }else if(opcion==2){//Mostrar Usuarios ascendente
-                    
-                    }
-            }else if(opcion==2){//Contador de digitos repetidos
+            break;
+        case 2://Menu Contador de digitos repetidos
             System.out.println("1.Ingresar numero");
             System.out.println("2.Ingresar numero a buscar");
             System.out.println("3.Mostrar numero de digitos");
             System.out.println("4.Menu principal");
                     opcion = entrada.nextInt();
-            }else if(opcion==3){//Tres numeros de mayor a menor
+            break;        
+        case 3://Menu Tres numeros de mayor a menor
             System.out.println("1.Ingresar numero");
             System.out.println("2.Mostrar ordenados");
             System.out.println("3.Menu principal");
                     opcion = entrada.nextInt();
-            }else if(opcion==4){//Calcular promedio(4notas de 6 estudiantes)
+            break;       
+         case 4://Menu Calcular promedio(4notas de 6 estudiantes)
                 System.out.println("1.opcion");
                 System.out.println("2.opcion");
-            }else if(opcion==5){//5.Salir
+            break;    
+         case 5://5.Salir
                 System.out.println("finalizado");
-            }else if(opcion>5){
+         case 6:
+         default:
                 System.out.println("opcion erronia debe ser de 1 - 5");
-            }
+            break;
+            }//fin switch menu principal
           
     }//Finn Tarea3
 }
